0cb7fb8a17a68522abe30e2b07228c58513696ad
Harsh Tripathi <harshtripathi042@gmail.com>
2025-03-25 00:30:06 +0530

    csrf-token in post

--- src/main/java/com/example/SpringSecurity/controller/HelloController.java
+++ src/main/java/com/example/SpringSecurity/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.SpringSecurity.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,4 +15,22 @@ public class HelloController {
         //  It is used to retrieve request-related information when handling HTTP requests in a Spring Boot project.
         return "Hello world "+ request.getSession().getId();
     }
+
+    // after login we have the sesison id
+    // some malicious program may try to steel the session id and login to our account
+    // this is called csrf
+    // to avoid this issue we should also provide token along with the request.
+
+    // get would be working even without csrf but post would be blocked
+
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    // get the token from spring security
+    // send the token in the post request
+    // or other way would be not to allow any one from the same site to access the session id
+
 }
